5d0ad6e8ed409c7ecc38e7d0ac23c8b6987c67f1
michaelskyf <skyflighter.kos@protonmail.com>
2021-12-29 13:23:56 +0100

    Add missing file 'test.c'

---
+++ test.c
@@ -0,0 +1,15 @@
+#include "selog.h"
+
+int main()
+{
+	log_setup_default();
+
+	log_trace("test");
+	log_debug("test");
+	log_info("test");
+	log_warning("test");
+	log_error("test");
+	log_fatal("test");
+
+	return 0;
+}
